e541c69ca492f340ae70fd085bf659daa064ed3e
Anton Astreiko <antonastreika@gmail.com>
2023-11-01 14:23:37 +0300

    fix linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    quotes: ['error', 'single'],
+    'array-bracket-spacing': 'error',
+    'object-curly-spacing': ['error', 'always'],
+  },
 };
